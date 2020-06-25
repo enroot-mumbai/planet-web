0a211f82251ac971c0170c00c12cd0a01f7d692c
Harsh Vitra <harshvitra@gmail.com>
2020-06-26 01:10:09 +0530

    installed mapbox

--- pages/map.js
+++ pages/map.js
@@ -1,9 +1,9 @@
 import Layout from '../src/features/common/Layout'
-
+import MapComponent from './../src/features/public/Map'
 export default function Map() {
   return (
     <Layout>
-      <p>Map</p>
+      <MapComponent />
     </Layout>
   )
 }
